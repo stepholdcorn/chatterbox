9fb7c8c2b2d63a8a14fa5428fb5aba6dc4bcafa2
Steph Oldcorn <stepholdcorn@Stephs-MacBook-Pro.local>
2014-12-08 15:56:02 +0000

    Refactoring code

--- chatbot.rb
+++ chatbot.rb
@@ -12,30 +12,30 @@ def bot_prompt
 end
 
 def get_response(input)
-
-	if input == "quit"
-	exit(0)
-	
-	else
-
+	exit(0) if input == "quit"
   	key = RESPONSES.keys.select {|k| /#{k}/ =~ input.downcase }.sample
   	/#{key}/ =~ input
   	bot_prompt
   	response = RESPONSES[key]
 	response.nil? ? 'sorry? I don\'t know much about that, can we talk about something else?' : response % { c1: $1, c2: $2, c3: $3}
+
+end
+
+def introduction
+	bot_prompt
+	puts "Hello, what's your name?".light_red
+	user_prompt
+	name = gets.chomp
+	bot_prompt
+	puts "Hello #{name}, what can I help you with?".light_red
+	user_prompt
+end
+def conversation
+	while(input = gets.chomp) do
+	  puts get_response(input).light_red
+	  user_prompt
 	end
-	
 end
 
-bot_prompt
-puts "Hello, what's your name?".light_red
-user_prompt
-name = gets.chomp
-bot_prompt
-puts "Hello #{name}, what can I help you with?".light_red
-user_prompt
-
-while(input = gets.chomp) do
-  puts get_response(input).light_red
-  user_prompt
-end
+introduction
+conversation
